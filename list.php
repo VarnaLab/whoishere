2f959c208f3f0787ad8ddce5330abd9e154a0aaf
Boris Filipov <obiwonn@gmail.com>
2012-02-04 15:10:25 +0200

    initial commit

---
+++ list.php
@@ -0,0 +1,27 @@
+<?php
+if (!function_exists("ssh2_connect")) die("function ssh2_connect doesn't exist");
+// log in at server1.example.com on port 22
+if(!($con = ssh2_connect("192.168.1.1", 22))){
+    echo "fail: unable to establish connection\n";
+} else {
+    // try to authenticate with username root, password secretpassword
+    if(!ssh2_auth_password($con, "", "")) {
+        echo "fail: unable to authenticate\n";
+    } else {
+ 
+        // execute a command
+        if (!($stream = ssh2_exec($con, "arp -a" ))) {
+            echo "fail: unable to execute command\n";
+        } else {
+            // collect returning data from command
+            stream_set_blocking($stream, true);
+            $data = "";
+            while ($buf = fread($stream,4096)) {
+                $data .= $buf;
+            }
+            fclose($stream);
+            echo $data;
+        }
+    }
+}
+?>
