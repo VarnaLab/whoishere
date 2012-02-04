60646d705af74e84b21f7eafe6c01578d4c239ea
Boris Filipov <obiwonn@gmail.com>
2012-02-04 15:13:32 +0200

    added configs

--- list.php
+++ list.php
@@ -1,11 +1,13 @@
 <?php
+require_once(dirname(__FILE__)."config.php");
+
 if (!function_exists("ssh2_connect")) die("function ssh2_connect doesn't exist");
 // log in at server1.example.com on port 22
 if(!($con = ssh2_connect("192.168.1.1", 22))){
     echo "fail: unable to establish connection\n";
 } else {
     // try to authenticate with username root, password secretpassword
-    if(!ssh2_auth_password($con, "", "")) {
+    if(!ssh2_auth_password($con, USER, PASSWORD)) {
         echo "fail: unable to authenticate\n";
     } else {
  
