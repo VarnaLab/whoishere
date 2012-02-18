4b57339478b4f07188af91a9bf6e09863719f980
Emil Mohamed <emil@2c-studio.com>
2012-02-18 18:51:27 +0200

    Security issue fix ... see github issue #1

--- bindAlias.php
+++ bindAlias.php
@@ -1,3 +1,13 @@
 <?php
-file_put_contents(dirname(__FILE__)."/data/".$_REQUEST['mac'], $_REQUEST['alias']);
+if (!isset($_REQUEST['mac'])) {
+	exit("No mac address supplied");
+}
+$mac = $_REQUEST['mac'];
+function is_valid_mac($mac) {
+	return preg_match('~^[A-F0-9\-:]*$~', $mac);
+}
+if (!is_valid_mac($mac)) {
+	exit("invalid mac address supplied");
+}
+file_put_contents(dirname(__FILE__) . "/data/" . $_REQUEST['mac'], $_REQUEST['alias']);
 ?>
